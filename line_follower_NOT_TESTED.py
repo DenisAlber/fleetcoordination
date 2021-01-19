1332743cad4200edcfb348d24512e07fd0f17d70
Denis Alber <56383734+DenisAlber@users.noreply.github.com>
2021-01-19 21:36:07 +0100

    Create line_follower_NOT_TESTED.py

---
+++ line_follower_NOT_TESTED.py
@@ -0,0 +1,24 @@
+from zumi.zumi import Zumi
+import time
+
+zumi = Zumi()
+
+zumi.reset_gyro()
+speed = 30
+heading = 0
+
+for x in range(40): # Take steps
+    
+    ir_readings = zumi.get_all_IR_data()
+    bottom_right_ir = ir_readings[1]
+    bottom_left_ir = ir_readings[3]
+
+    if bottom_right_ir > 100 and bottom_left_ir > 100:
+        heading = heading           # do nothing
+    elif bottom_right_ir < 100:
+        heading += 10               # turn right
+    elif bottom_left_ir < 100:
+        heading -= 10               # turn left
+    zumi.go_straight(speed, heading)
+
+zumi.stop()
