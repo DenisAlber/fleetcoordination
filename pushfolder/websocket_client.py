3f07138a6c21d56a9dc44f0c8388a148e9a04976
Denis Alber <56383734+DenisAlber@users.noreply.github.com>
2021-06-10 17:50:49 +0200

    Update websocket_client.py

--- pushfolder/websocket_client.py
+++ pushfolder/websocket_client.py
@@ -16,27 +16,18 @@ def on_message(ws, message):
 def on_close(ws):
     print("### closed ###")
 
-# def LongPolling():
-#     # url = 'http://192.168.10.80:3000/Zumi'
-#     url = 'http://192.168.178.108:3000/Zumi'
- 
-#     x = requests.get(url)
-#     data = json.loads(x.text)
-#     print(data.get("id") + " " + str(data.get("isBlocked")))
-#     LongPolling()
-
-
 def SendMessageThread():
-    for x in range(100, 150):
+    while True:
         # print(x)
         ws.send('{"node1":"A", "node2":"B"}')
         time.sleep(1)
 
 
 websocket.enableTrace(True)
-# ws = websocket.WebSocketApp("wss://fleetcoordination-zumi-cars.herokuapp.com/", on_message = on_message, on_close = on_close)
+ws = websocket.WebSocketApp("wss://fleetcoordination-zumi-cars.herokuapp.com/", on_message = on_message, on_close = on_close)
+# ws = websocket.WebSocketApp("ws://localhost:3000", on_message = on_message, on_close = on_close)
 
-ws = websocket.WebSocketApp("ws://localhost:3000", on_message = on_message, on_close = on_close)
+# Start websocket client
 wst = threading.Thread(target=ws.run_forever)
 wst.daemon = True
 wst.start()
@@ -48,7 +39,7 @@ while not ws.sock.connected and conn_timeout:
     conn_timeout -= 1
 
 # thread = threading.Thread(target = LongPolling)
-thread2 = threading.Thread(target = WaitThread)
+thread2 = threading.Thread(target = SendMessageThread)
 # thread.start()
 thread2.start()
 print("wait...")
